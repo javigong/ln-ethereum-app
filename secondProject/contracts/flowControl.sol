2d98d320d1046c31a0eeb4b21288a797c59413f5
Javier Gongora <jgongora@gmail.com>
2022-06-28 16:32:09 -0700

    Add smart contract demoing control flow with conditions and iterations

---
+++ secondProject/contracts/flowControl.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/*
+ * @title Solidity data types
+ * @author Michael Solomon
+ * @notice A simply smart contract to demonstrate control flow with conditions and iterations in Solidity
+ *
+ */
+
+contract FlowControl {
+    function calcDozenDiscountIF(uint256 purchasedQty)
+        private
+        pure
+        returns (bool)
+    {
+        bool giveDozenPrice = false;
+
+        if (purchasedQty >= 12) {
+            giveDozenPrice = true; //additional code can be placed here
+        } else {
+            giveDozenPrice = false;
+        }
+
+        return (giveDozenPrice);
+    }
+
+    function calcDozenDiscountWHILE(uint256 purchasedQty)
+        private
+        pure
+        returns (bool)
+    {
+        bool giveDozenPrice = false;
+        uint256 numDonuts = 1;
+
+        while (numDonuts < purchasedQty) {
+            numDonuts++;
+            if (numDonuts >= 12) {
+                giveDozenPrice = true;
+                break;
+            }
+        }
+
+        return (giveDozenPrice);
+    }
+
+    function calcDozenDiscountDO(uint256 purchasedQty)
+        private
+        pure
+        returns (bool)
+    {
+        bool giveDozenPrice = false;
+        uint256 numDonuts = 1;
+
+        do {
+            numDonuts++;
+            if (numDonuts >= 12) {
+                giveDozenPrice = true;
+                break;
+            }
+        } while (numDonuts < purchasedQty);
+
+        return (giveDozenPrice);
+    }
+
+    function calcDozenDiscountFOR(uint256 purchasedQty)
+        private
+        pure
+        returns (bool)
+    {
+        bool giveDozenPrice = false;
+        uint256 numDonuts = 1;
+
+        for (numDonuts = 1; numDonuts <= purchasedQty; numDonuts++) {
+            if (numDonuts >= 12) {
+                giveDozenPrice = true;
+                break;
+            }
+        }
+
+        return (giveDozenPrice);
+    }
+}
