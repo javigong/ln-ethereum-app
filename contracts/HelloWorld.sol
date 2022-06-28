1522254f03dd657fb8ec9b0b0d181d19b1b2ced4
Javier Gongora <jgongora@gmail.com>
2022-06-28 12:07:31 -0700

    add simple smart contract and deploy

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string private helloMessage = "Hello world";
+
+    function getHelloMessage() public view returns (string memory) {
+        return helloMessage;
+    }
+}
